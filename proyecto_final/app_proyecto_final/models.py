37717ae44c8fdb020a176027d73602bf292b54b2
Marcos Gonzalez <gonzalezmarcos_90@hotmail.com>
2023-04-03 21:03:28 -0300

    Agregando str a modelos

--- proyecto_final/app_proyecto_final/models.py
+++ proyecto_final/app_proyecto_final/models.py
@@ -5,12 +5,18 @@ from django.db import models
 # Create your models here.
 class Curso(models.Model):
     nombre = models.CharField(max_length=50)
-    comision = models.IntegerField()       
+    comision = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.nombre} - {self.comision}"   
 
 class Estudiante(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.nombre} - {self.apellido}"  
 
 class Profesor(models.Model):
     nombre = models.CharField(max_length=30)
@@ -18,7 +24,13 @@ class Profesor(models.Model):
     email = models.EmailField()
     profesion = models.CharField(max_length=30)
 
+    def __str__(self):
+        return f"{self.nombre} - {self.apellido}"  
+
 class Entregable(models.Model):
     nombre = models.CharField(max_length=50)
     fecha_entrega = models.DateField()
     entregado = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.nombre} - {self.fecha_entrega}"  
